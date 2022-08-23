1efbf6dc15dff69f380bce5fb5eef3b811b0aeae
Martin <curtis.martin@nih.gov>
2022-08-23 18:04:56 -0400

    Update input format to include json versus command line prompts

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 Module for analyzing omics data written in Python.
 Will provide classes & functions for proteomics analysis, to start. 
 
-### Env
-Contains conda environment required.  
+## `dpea.py`
+Currently performs differential protein expression analysis using mass spectrometry data. 
+Highly suggest using `params.json` file as input; doing so provides reproducibility & is generally simpler. 
+However code can be used in command line w the following options:
+- `-i` indicates a json input data file.
+- `-f` points to the mass spec data file & indicates further user options required. 
+Working to generalize &, eventually, convert to python module. 
+
+## Exp
+Contains json files w script parameters for each experiment. 
+
+## Env
+Contains conda environment yml file.  
--- dpea.py
+++ dpea.py
@@ -19,7 +19,8 @@ import sys
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
-import statsmodels.stats.multitest as smsm # only to check adjusted p-values, for now
+import statsmodels.stats.multitest as smsm # to calculate BH-adjusted p-values
+import json # for reading experiment parameters
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -27,80 +28,112 @@ import seaborn as sns
 
 ##### MAXIMIZE USER INPUT TO IMPROVE WORKFLOW GENERALIZATION
 #----------------------------------------------------------------------------#
-### get & format user inputs
-filepath = os.path.abspath(sys.argv[1])
-dict_args = dict(arg.split('=') for arg in sys.argv[2:])
-
-### set confidence for determining "statistical significance"
-if '--alpha' in dict_args.keys():
-    alpha = float(dict_args['--alpha'])
-    print(f'\nAlpha = {alpha:0.2f}...')
-else:
-    alpha = 0.05
-    print(f'\nSetting alpha to {alpha:0.2f}...')
+dict_args = dict(arg.split('=') for arg in sys.argv[1:])
 
 
 ### NEW FEATURE: IMPORT PARAMETER FILE FOR SMOOTHER PROCESSING
+if '-i' in dict_args.keys():
+    dict_params = json.load(open(dict_args['-i']))
 
-
+### set confidence level for "statistical significance"
+    alpha = float(dict_params['Parameters']['alpha'])
+    print(f'\nAlpha = {alpha:0.2f}...')
+    
 ### set threshold for determining "high fold-change"
-if '--fc' in dict_args.keys():
-    fcthresh = float(dict_args['--fc'])
+    fcthresh = float(dict_params['Parameters']['fcthresh'])
     print(f'Fold-change = {fcthresh:0.2f}...')
-else:
-    fcthresh = 2.0
-    print(f'Setting fold-change threshold to {fcthresh:0.2f}...')
-
+    
 ### set flag for labeling points (0 means no label; 1 means label)
-if '--label' in dict_args.keys():
-    label_flag = 0
-    print('Not labeling points...')
+    label_flag = int(dict_params['Parameters']['label'])
+
+### get columns required for analysis
+    l_psmcols1 = dict_params['Treatments']['1']
+    l_psmcols2 = dict_params['Treatments']['2']
+
+### get column for normalizing PSMs against protein length
+    plen = dict_params['Metadata']['Lengths']
+    
+### get IO variables
+    name_inpu = os.path.split(dict_params['Metadata']['Data'])[-1].split('.')[0]
+    name_sheet = dict_params['Metadata']['Sheet']
+    path_data = os.path.split(dict_params['Metadata']['Data'])[0]
+    path_inpu = os.path.join(os.path.split(os.path.abspath(sys.argv[0]))[0], dict_params['Metadata']['Data'])
+    name_outp = dict_params['Metadata']['Name']
+
+### load data
+    data = pd.read_excel(io=path_inpu, sheet_name=name_sheet)
+    
+    
+### otherwise provide inputs via bash
 else:
-    label_flag = 1
+    path_inpu = os.path.abspath(dict_args['-f'])
+    print(path_inpu)
+    
+### set confidence level for "statistical significance"
+    if '--alpha' in dict_args.keys():
+        alpha = float(dict_args['--alpha'])
+        print(f'\nAlpha = {alpha:0.2f}...')
+    else:
+        alpha = 0.05
+        print(f'\nSetting alpha to {alpha:0.2f}...')
+    
+### set threshold for determining "high fold-change"
+    if '--fc' in dict_args.keys():
+        fcthresh = float(dict_args['--fc'])
+        print(f'Fold-change = {fcthresh:0.2f}...')
+    else:
+        fcthresh = 2.0
+        print(f'Setting fold-change threshold to {fcthresh:0.2f}...')
+    
+### set flag for labeling points (0 means no label; 1 means label)
+    if '--label' in dict_args.keys():
+        label_flag = 0
+        print('Not labeling points...')
+    else:
+        label_flag = 1
 
 ### get filename & path to data from user input
-# datafiles = pd.Series(os.listdir(path_data))
-# filename = datafiles.loc[int(input(f'\n{datafiles}\n\nSelect index of input file for analysis: '))].split('.')[0]
-filename = os.path.split(filepath)[-1].split('.')[0]
-path_data = os.path.split(filepath)[0]
+    name_inpu = os.path.split(path_inpu)[-1].split('.')[0]
+    path_data = os.path.split(path_inpu)[0]
 
 ### have the user select which sheet in data
-datasheets = pd.Series(pd.ExcelFile(filepath).sheet_names)
-sheetname = datasheets.loc[int(input(f'\n{datasheets}\n\nSelect index of sheet to use for analysis:\t'))]
+    datasheets = pd.Series(pd.ExcelFile(path_inpu).sheet_names)
+    name_sheet = datasheets.loc[int(input(f'\n{datasheets}\n\nSelect index of sheet to use for analysis:\t'))]
+    
+### identify condition columns either through script call or via interactive input
+    if '--cond1' in dict_args.keys():
+        l_psmcols1 = dict_args['--cond1'].split(',')
+    else:
+        l_psmcols1 = input(f'\n{pd.Series(data.columns)}\n\nSelect PSM data for first sample:\t\t').split()
+    
+    if '--cond2' in dict_args.keys():
+        l_psmcols2 = dict_args['--cond2'].split(',')
+    else:
+        l_psmcols2 = input(f'\n{pd.Series(data.columns)}\n\nSelect PSM data for second sample:\t\t').split()
+
+### load data
+    data = pd.read_excel(io=path_inpu, sheet_name=name_sheet)
+        
+### convert indices to integers
+    l_psmcols1 = [int(i) for i in l_psmcols1]
+    l_psmcols2 = [int(i) for i in l_psmcols2]
+    
+### convert indices to columns
+    l_psmcols1 = data.columns[l_psmcols1]
+    l_psmcols2 = data.columns[l_psmcols2]
+    
+### get desired name for output files
+    name_outp = input('Provide name for output files:\t\t\t')
 #----------------------------------------------------------------------------#
 
 
 ##### LOAD, CLEAN, & SELECT DATA
 #----------------------------------------------------------------------------#
-### load data
-data = pd.read_excel(io=filepath, sheet_name=sheetname)
-
 ### define & make output directory... helps keep things organized!
-path_outp = os.path.join(os.getcwd(), 'Output', filename)
+path_outp = os.path.join(os.getcwd(), 'Output', name_inpu)
 if not os.path.exists(path_outp):
     os.mkdir(path_outp)
 
-### identify condition columns either through script call or via interactive input
-if '--cond1' in dict_args.keys():
-    l_psmcols1 = dict_args['--cond1'].split(',')
-else:
-    l_psmcols1 = input(f'\n{pd.Series(data.columns)}\n\nSelect PSM data for first sample:\t\t').split()
-
-if '--cond2' in dict_args.keys():
-    l_psmcols2 = dict_args['--cond2'].split(',')
-else:
-    l_psmcols2 = input(f'\n{pd.Series(data.columns)}\n\nSelect PSM data for second sample:\t\t').split()
-
-### get desired name for output files
-oname = input('Provide name for output files:\t\t\t')
-
-### convert indices to integers
-l_psmcols1 = [int(i) for i in l_psmcols1]
-l_psmcols2 = [int(i) for i in l_psmcols2]
-
-### convert indices to columns
-l_psmcols1 = data.columns[l_psmcols1]
-l_psmcols2 = data.columns[l_psmcols2]
 
 ### select data of interest
 df1 = data[l_psmcols1].copy()
@@ -149,16 +182,14 @@ def normalize(df, data, col, plen='# AAs'):
     df_norm = pd.DataFrame()
     df_norm[f'{col}_SAF'] = df[col] / df['# AAs'] # spectral counts over protein length
     df_norm[f'{col}_nSAF'] = df_norm[f'{col}_SAF'] / df_norm[f'{col}_SAF'].sum() # normalized SAF 
-#     df_norm[f'{col}_nSAF'] = df_norm[f'{col}_SAF'] / df_norm[f'{col}_SAF'].max() # THIS IS WRONG!!!
-#     print(df_norm[f'{col}_nSAF'].sum())
     
     return(df_norm)
 
 ### normalize PSM --> nSAF
 for col in df1.columns:
-    df1 = df1.join(normalize(df1, data, col))
+    df1 = df1.join(normalize(df1, data, col, plen=plen))
 for col in df2.columns:
-    df2 = df2.join(normalize(df2, data, col))
+    df2 = df2.join(normalize(df2, data, col, plen=plen))
     
 ### get nSAF columns for statistical tests & plotting later
 l_nsafcols1 = [col for col in df1.columns if 'nSAF' in col]
@@ -194,49 +225,49 @@ print(f'\nBonferroni-adjusted significance level:\t{smsm.multipletests(df_sub["p
 #----------------------------------------------------------------------------#
 ### filter data to mitochondrial proteins only... MAY NEED TO MAKE MORE FLEXIBLE, DEPENDS ON DATA CONSISTENCY & RELEVANCE
 # f_mito = 'Mitochondrial' 
-# df_vol = data[data['Mitochondrial location'] == f_mito].copy()
-df_vol = data.copy()
+# df_volc = data[data['Mitochondrial location'] == f_mito].copy()
+df_volc = data.copy()
 
 ### create new frame for volcano plot
-df_vol = df_vol[['Accession','GenSymbol']].join(df_sub)
+df_volc = df_volc[['Accession','GenSymbol']].join(df_sub)
 
 ### calculate log2(FC) = log2 of fold-change
-df_vol['Mean_Grp1'] = df_vol[l_nsafcols1].mean(axis=1)
-df_vol['Mean_Grp2'] = df_vol[l_nsafcols2].mean(axis=1)
-df_vol['FC'] = df_vol['Mean_Grp2'] / df_vol['Mean_Grp1'] # interpreting FC to mean from grp1 --> grp2
+df_volc['Mean_Grp1'] = df_volc[l_nsafcols1].mean(axis=1)
+df_volc['Mean_Grp2'] = df_volc[l_nsafcols2].mean(axis=1)
+df_volc['FC'] = df_volc['Mean_Grp2'] / df_volc['Mean_Grp1'] # interpreting FC to mean from grp1 --> grp2
 
 ### calculate values for volcano plot (log2(FC) & log10(p-value))
-df_vol['log2(FC)'] = np.log2(df_vol['FC'])
-df_vol['-log10(p)'] = -np.log10(df_vol['p-value'])
-df_vol['-log10(p-adj)'] = -np.log10(df_vol['p-adj'])
+df_volc['log2(FC)'] = np.log2(df_volc['FC'])
+df_volc['-log10(p)'] = -np.log10(df_volc['p-value'])
+df_volc['-log10(p-adj)'] = -np.log10(df_volc['p-adj'])
 
 ### add flag for "statistical significance" based on user-defined alpha
-df_vol.loc[df_vol[df_vol['p-value'] <= alpha].index, 'p-Sig'] = 1
-df_vol.loc[df_vol[df_vol['p-value'] > alpha].index, 'p-Sig'] = 0
+df_volc.loc[df_volc[df_volc['p-value'] <= alpha].index, 'p-Sig'] = 1
+df_volc.loc[df_volc[df_volc['p-value'] > alpha].index, 'p-Sig'] = 0
 
 ## add flag for "statistical significance" based on user-defined BH-corrected alpha
-df_vol.loc[df_vol[df_vol['p-adj'] <= alpha].index, 'BH-Sig'] = 1
-df_vol.loc[df_vol[df_vol['p-adj'] > alpha].index, 'BH-Sig'] = 0
+df_volc.loc[df_volc[df_volc['p-adj'] <= alpha].index, 'BH-Sig'] = 1
+df_volc.loc[df_volc[df_volc['p-adj'] > alpha].index, 'BH-Sig'] = 0
 
 ### add flag for "high fold-change" based on user-defined threshold
 fcthresh_log2 = np.log2(fcthresh)
-df_vol.loc[df_vol[np.abs(df_vol['log2(FC)']) > fcthresh_log2].index, 'HighFC'] = 1 
-df_vol.loc[df_vol[np.abs(df_vol['log2(FC)']) <= fcthresh_log2].index, 'HighFC'] = 0 
+df_volc.loc[df_volc[np.abs(df_volc['log2(FC)']) > fcthresh_log2].index, 'HighFC'] = 1 
+df_volc.loc[df_volc[np.abs(df_volc['log2(FC)']) <= fcthresh_log2].index, 'HighFC'] = 0 
 
 ### add flag for high fold-change & high significance, based on user definitions
-# df_vol.loc[df_vol[df_vol[['HighFC', 'p-Sig']].sum(axis=1) == 2].index, 'POI'] = 1
-# df_vol.loc[df_vol[df_vol[['HighFC', 'p-Sig']].sum(axis=1) != 2].index, 'POI'] = 0
+# df_volc.loc[df_volc[df_volc[['HighFC', 'p-Sig']].sum(axis=1) == 2].index, 'POI'] = 1
+# df_volc.loc[df_volc[df_volc[['HighFC', 'p-Sig']].sum(axis=1) != 2].index, 'POI'] = 0
 
 ### join to rest of data, then sort for simpler selection of important genes
-df_vol = df_vol.join(data.loc[:, [col for col in data.columns if col not in df_vol.columns]])
-df_vol = df_vol.sort_values(['BH-Sig', 'p-Sig', 'HighFC', '-log10(p-adj)', 'FC'], ascending=False).reset_index(drop=True)
+df_volc = df_volc.join(data.loc[:, [col for col in data.columns if col not in df_volc.columns]])
+df_volc = df_volc.sort_values(['BH-Sig', 'p-Sig', 'HighFC', '-log10(p-adj)', 'FC'], ascending=False).reset_index(drop=True)
 
 ### add column for defining hue later
-df_vol.loc[df_vol[(df_vol['HighFC'] == 1) & (df_vol['p-Sig'] == 0)].index, 'Hue'] = 1
-df_vol.loc[df_vol[(df_vol['HighFC'] == 0) & (df_vol['p-Sig'] == 1)].index, 'Hue'] = 2
-df_vol.loc[df_vol[(df_vol['HighFC'] == 1) & (df_vol['p-Sig'] == 1)].index, 'Hue'] = 3
-df_vol.loc[df_vol[(df_vol['HighFC'] == 1) & (df_vol['BH-Sig'] == 1)].index, 'Hue'] = 4
-df_vol['Hue'] = df_vol['Hue'].fillna(0)
+df_volc.loc[df_volc[(df_volc['HighFC'] == 1) & (df_volc['p-Sig'] == 0)].index, 'Hue'] = 1
+df_volc.loc[df_volc[(df_volc['HighFC'] == 0) & (df_volc['p-Sig'] == 1)].index, 'Hue'] = 2
+df_volc.loc[df_volc[(df_volc['HighFC'] == 1) & (df_volc['p-Sig'] == 1)].index, 'Hue'] = 3
+df_volc.loc[df_volc[(df_volc['HighFC'] == 1) & (df_volc['BH-Sig'] == 1)].index, 'Hue'] = 4
+df_volc['Hue'] = df_volc['Hue'].fillna(0)
 #----------------------------------------------------------------------------#
 
 
@@ -246,7 +277,7 @@ df_vol['Hue'] = df_vol['Hue'].fillna(0)
 xmin = -5
 xmax = 5
 ymin = 0
-ymax = np.max(df_vol['-log10(p)']) + 0.1
+ymax = np.max(df_volc['-log10(p)']) + 0.1
 
 ### generate plot parameters based on user-defined parameters above
 xmin_alpha = 0.20
@@ -261,84 +292,77 @@ palette = {0:'#eff3ff', 1:'#bdd7e7', 2:'#6baed6', 3:'#3182bd', 4:'#08519c'} # bl
 markers = {0:'o', 1:'h', 2:'X', 3:'d', 4:'P'}
 fig, ax = plt.subplots(1, 1, figsize=(8,6))
 
-sns.scatterplot(x='log2(FC)', y='-log10(p)', hue='Hue', style='Hue', data=df_vol, palette=palette, markers=markers, edgecolor='black', linewidth=0.25, s=16, legend=False, ax=ax)
+sns.scatterplot(x='log2(FC)', y='-log10(p)', hue='Hue', style='Hue', data=df_volc, palette=palette, markers=markers, edgecolor='black', linewidth=0.25, s=18, legend=False, ax=ax)
 if label_flag == 1:
-    print('\nWorking on the labels. They can take a minute...\n')
-    for idx in range(df_vol[df_vol['p-Sig'] == 1].shape[0])[:100]:
-        x = 0.075+df_vol.loc[idx, 'log2(FC)']
-        y = df_vol.loc[idx, '-log10(p)']
-        s = df_vol.loc[idx, 'GenSymbol']
+    print('\nWorking on the labels. Can take a minute...\n')
+    for idx in range(df_volc[df_volc['p-Sig'] == 1].shape[0])[:100]:
+        x = 0.075+df_volc.loc[idx, 'log2(FC)']
+        y = df_volc.loc[idx, '-log10(p)']
+        s = df_volc.loc[idx, 'GenSymbol']
         plt.text(x=x, y=y, s=s, fontdict=dict(color='k', size=4))
 
 ax.set_xlim([xmin, xmax])
 ax.set_xlabel('$log_{2}$(Fold Change)')
 ax.set_ylim([ymin, ymax])
 ax.set_ylabel('$-log_{10}$(p-value)')
-plt.title(f'Volcano Plot ({oname})')
+plt.title(f'Volcano Plot ({name_outp})')
 
 if label_flag == 1:
-    plt.savefig(f'{path_outp}/{oname}_volcano.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'{path_outp}/{name_outp}_volcano.png', bbox_inches='tight', dpi=300)
 else:
-    plt.savefig(f'{path_outp}/{oname}_volcano-nolabel.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'{path_outp}/{name_outp}_volcano-nolabel.png', bbox_inches='tight', dpi=300)
 # plt.show()
 
 ### reorder columns & write data to file
 if label_flag == 1:
     l_impcols = ['Accession', 'GenSymbol', 'FC', 'log2(FC)', 'p-value', '-log10(p)', 'p-adj', '-log10(p-adj)', 'Protein names', 'HighFC', 'p-Sig', 'BH-Sig']
-    df_vol = df_vol[l_impcols].join(df_vol.loc[:, ~df_vol.columns.isin(l_impcols)])
-    df_vol.to_excel(f'{path_outp}/{oname}_processed.xlsx', index=False)
-
+    df_volc = df_volc[l_impcols].join(df_volc.loc[:, ~df_volc.columns.isin(l_impcols)])
+    df_volc.to_excel(f'{path_outp}/{name_outp}_processed.xlsx', index=False)
 
 ### make a volcano plot w/ adjusted p-values
-# ymax = np.max(df_vol['-log10(p-adj)']) + 0.1
+# ymax = np.max(df_volc['-log10(p-adj)']) + 0.1
 
 # palette = {1:'firebrick', 0:'Silver'}
 # fig, ax = plt.subplots(1, 1, figsize=(8,6))
 
-# sns.scatterplot(x='log2(FC)', y='-log10(p-adj)', hue='Hue', style='Hue', data=df_vol, palette=palette, markers=markers, edgecolor='black', linewidth=0.25, s=15, legend=False, ax=ax)
+# sns.scatterplot(x='log2(FC)', y='-log10(p-adj)', hue='Hue', style='Hue', data=df_volc, palette=palette, markers=markers, edgecolor='black', linewidth=0.25, s=15, legend=False, ax=ax)
 # if label_flag == 1:
 #     print('\nWorking on the labels. They can take a minute...\n')
-#     for idx in range(df_vol[df_vol['p-Sig'] == 1].shape[0])[:100]:
-#         x = 0.075+df_vol.loc[idx, 'log2(FC)']
-#         y = df_vol.loc[idx, '-log10(p)']
-#         s = df_vol.loc[idx, 'GenSymbol']
+#     for idx in range(df_volc[df_volc['p-Sig'] == 1].shape[0])[:100]:
+#         x = 0.075+df_volc.loc[idx, 'log2(FC)']
+#         y = df_volc.loc[idx, '-log10(p)']
+#         s = df_volc.loc[idx, 'GenSymbol']
 #         plt.text(x=x, y=y, s=s, fontdict=dict(color='k', size=4))
 
 # ax.set_xlim([xmin, xmax])
 # ax.set_xlabel('$log_{2}$(Fold Change)')
 # ax.set_ylim([ymin, ymax])
 # ax.set_ylabel('$-log_{10}$(p-value)')
-# plt.title(f'Volcano Plot ({oname})')
+# plt.title(f'Volcano Plot ({name_outp})')
 
 # ax.set_xlim([xmin, xmax])
 # ax.set_xlabel('$log_{2}$(Fold Change)')
 # ax.set_ylim([ymin, ymax])
 # ax.set_ylabel('$-log_{10}$(Adjusted p-value)')
-# plt.title(f'Volcano Plot ({oname})')
+# plt.title(f'Volcano Plot ({name_outp})')
 
 # if label_flag == 1:
-#     plt.savefig(f'{path_outp}/{oname}_volcano-adj.png', bbox_inches='tight', dpi=300)
+#     plt.savefig(f'{path_outp}/{name_outp}_volcano-adj.png', bbox_inches='tight', dpi=300)
 # else:
-#     plt.savefig(f'{path_outp}/{oname}_volcano-adj-nolabel.png', bbox_inches='tight', dpi=300)
+#     plt.savefig(f'{path_outp}/{name_outp}_volcano-adj-nolabel.png', bbox_inches='tight', dpi=300)
 # plt.show()
 
 ### create frame for printing to output
-df_outp = df_vol.sort_values('FC').copy()
+df_outp = df_volc.sort_values('FC').copy()
 
 ### provide reference list for performing analyses against
-df_outp['Accession'].to_csv(f'{path_outp}/{oname}_ref.txt', sep='\t', header=False, index=False)
+df_outp['Accession'].to_csv(f'{path_outp}/{name_outp}_ref.txt', sep='\t', header=False, index=False)
 
 ### provide ranked list of proteins for use w overrepresentation analysis (ORA) in PANTHER
-df_outp[df_outp['HighFC'] == 1]['Accession'].to_csv(f'{path_outp}/{oname}_ora.txt', sep='\t', header=False, index=False)
+df_outp[df_outp['HighFC'] == 1]['Accession'].to_csv(f'{path_outp}/{name_outp}_ora.txt', sep='\t', header=False, index=False)
 
 ### provide ranked list of proteins for use w enrichment (FCA) analysis in PANTHER
-df_outp[['Accession', 'FC']].dropna().to_csv(f'{path_outp}/{oname}_sea.txt', sep='\t', header=False, index=False)
-# df_outp.to_string(f'{path_outp}/{oname}_ranked.txt', header=False, index=False)
-# with open(f'{path_outp}/{oname}_ranked.txt', 'w') as text:
-#     for idx, entry in df_outp.iterrows():
-#         entryA = entry['Accession']
-#         entryB = entry['FC']
-#         text.write(f'{entryA}\t{entryB}\n')
+df_outp[['Accession', 'FC']].dropna().to_csv(f'{path_outp}/{name_outp}_sea.txt', sep='\t', header=False, index=False)
 #----------------------------------------------------------------------------#  
 
 
